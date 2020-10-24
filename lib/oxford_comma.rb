91e35c5b3ae3fd24f590942129d3ff352e531e99
Diana Mora <morra.dianna@gmail.com>
2020-10-23 20:53:22 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,41 @@
-def oxford_comma(array)
+# def oxford_comma(array)
+#     return array.join
+# end
 
-end
+# def oxford_comma(array)
+#     if array.size == 1
+#         array.join
+#     elsif 
+#         array.size == 2
+#         return array.join(" and ")
+#     elsif 
+#         array.size >= 3
+#         counter = 0
+#         array2 = []
+#         until counter == array.length-1
+#             array2.push(array[counter] + ", ")
+#             counter += 1
+#         end    
+#             array2.push("and " + array[counter])
+#         return array2.join
+        
+#     end
+# end    
+
+def oxford_comma(array)
+    if array.size == 1
+        array.join
+    elsif array.size == 2
+        return array.join(" and ")
+    elsif array.size >= 3
+        counter = 0
+        array2 = []
+        until counter == array.length-1
+            array2.push(array[counter] + ", ")
+            counter += 1
+        end    
+        array2.push("and " + array[counter])
+        return array2.join
+        
+    end
+end    
