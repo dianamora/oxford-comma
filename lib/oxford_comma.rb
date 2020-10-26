39c6c687eb227ae3bd031109440d602a0e296a44
Diana Mora <morra.dianna@gmail.com>
2020-10-26 10:47:36 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -39,3 +39,16 @@ def oxford_comma(array)
         
     end
 end    
+
+# def oxford_comma(arr)
+#     case arr.size
+#     when 0
+#       ""
+#     when 1
+#       arr.first
+#     when 2
+#       arr.join(' and ')
+#     else
+#       [arr[0..-2].join(', '), arr.last].join(', and ')
+#     end
+#   end
